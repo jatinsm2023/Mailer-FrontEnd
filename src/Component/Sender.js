929fa58a041ee4bf15e701a952667293f9f2572b
Jatin Mahawar <jatinmahawar08@kgpian.iitkgp.ac.in>
2024-05-31 09:53:38 +0530

    File Attachement added

--- src/Component/Sender.js
+++ src/Component/Sender.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
 function Sender() {
   const [file, setFile] = useState(null);
   const [emails, setEmails] = useState([]);
+  const [secondFiles, setSecondFiles] = useState([]);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
-
+  const handleSecondFileChange = (event) => {
+    setSecondFiles(Array.from(event.target.files));
+  };
   const handlesend = async () => {
     if (file) {
       const reader = new FileReader();
@@ -27,41 +30,53 @@ function Sender() {
 
     // take subject from id subject
     const subject = document.getElementById("subject").value;
-    const email = document.getElementById("email").value;
-    const emailpassword = document.getElementById("apppassword").value
+    const EMAIL = document.getElementById("email").value;
+    const EMAIL_APP_PASSWORD = document.getElementById("apppassword").value;
     // take message from id message
     const message = document.getElementById("message").value;
 
-    if (!subject || !email || !emailpassword || !message || emails.length === 0) {
-      toast.error('Please fill in all fields');
+    if (
+      !subject ||
+      !EMAIL ||
+      !EMAIL_APP_PASSWORD ||
+      !message ||
+      emails.length === 0
+    ) {
+      toast.error("Please fill in all fields");
       return;
     }
-    
+
+    const formData = new FormData();
+
+    // Append the files to the FormData instance
+    secondFiles.forEach((file, index) => {
+      formData.append(`file${index}`, file);
+    });
+    // Append other data to the FormData instance
+    formData.append("EMAIL", EMAIL);
+    formData.append("EMAIL_APP_PASSWORD", EMAIL_APP_PASSWORD);
+    formData.append("RECIEVER_MAILS", JSON.stringify(emails));
+    formData.append("MAIL_SUBJECT", subject);
+    formData.append("MAIL_BODY", message);
+
+  
+    // https://mailer-backend-h8rh.onrender.com
     // now send file,subject, message in the backend use the fetch api https://mailer-backend-h8rh.onrender.com/api//product/getbill
     const response = await fetch("https://mailer-backend-h8rh.onrender.com/api//product/getbill", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        emailpassword : emailpassword,
-        usermail: emails,
-        mailsubject: subject,
-        mailbody: message,
-      }),
+      body: formData,
     });
 
     if (response.status === 201) {
-     toast.success("Mail Sent Successfully")
+      toast.success("Mail Sent Successfully");
     } else {
-      toast.error("Some Error Occured")
+      toast.error("Some Error Occured");
     }
   };
 
   return (
     <>
-    <Toaster/>
+      <Toaster />
       <div className="isolate bg-white px-6 py-6 sm:py-6 lg:px-8">
         <div
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
@@ -142,9 +157,26 @@ function Sender() {
               <div className="mt-2.5">
                 <input
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  id="file_input"
+                  id="file_input1"
                   type="file"
                   onChange={handleFileChange}
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                className="block text-sm font-semibold leading-6 text-gray-900"
+                htmlFor="file_input"
+              >
+                Upload file ( Attachments in the Mail )
+              </label>
+              <div className="mt-2.5">
+                <input
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="file_input"
+                  type="file"
+                  multiple
+                  onChange={handleSecondFileChange}
                 />
               </div>
             </div>
